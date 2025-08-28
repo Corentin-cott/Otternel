2ac5b7adb2e33942f62e753993c83f5e20c96794
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2025-08-28 14:41:00 +0200

    Initial commit for Otternel

---
+++ src/config.rs
@@ -0,0 +1,20 @@
+// We use `serde` to easily map environment variables into a Rust struct
+use serde::Deserialize;
+
+// Each field corresponds to one environment variable
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub database_url: String,
+    pub serverlog_folder: String,
+}
+
+impl Config {
+    // This function loads the .env file and deserializes the environment variables into a Config struct
+    pub fn from_env() -> Result<Self, envy::Error> {
+        // Load variables from a `.env` file if present
+        dotenvy::dotenv().ok();
+
+        // Use `envy` to deserialize environment variables into the Config struct. It automatically matches variable names to struct fields
+        envy::from_env()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+
+mod config; // Import config module
+
+fn main() {
+    // Try to load configuration from environment variables
+    match config::Config::from_env() {
+        // Ok = If successful
+        Ok(cfg) => {
+            println!("Config loaded successfully:");
+            println!("DATABASE_URL: {}", cfg.database_url);
+            println!("SERVERLOG_FOLDER: {}", cfg.serverlog_folder);
+        }
+        // Err = If failed
+        Err(err) => {
+            eprintln!("Failed to load config: {}", err);
+        }
+    }
+}
